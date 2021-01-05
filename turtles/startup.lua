93c2f390f548de153fff8bc17627e4dd39a69b24
buffaloniandev <76931100+buffaloniandev@users.noreply.github.com>
2021-01-05 00:10:50 -0500

    Add basic update script

--- turtles/startup.lua
+++ turtles/startup.lua
@@ -1,1 +1,15 @@
-print("Startup success.")
+script_url = "https://raw.githubusercontent.com/buffaloniandev/cc_programs/main/turtles/startup.lua"
+
+script_handle = http.get(script_url)
+if not script_handle then
+  print("Update check failed.")
+else
+  print("Updating...")
+  script_content = script_handle.readAll()
+  script_handle.close()
+  startup_handle = fs.open("/startup.lua", 'w')
+  startup_handle.write(script_content)
+  startup_handle.close()
+  print("Updated.")
+end
+print("Startup complete.")
