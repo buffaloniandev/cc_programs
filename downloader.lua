8baa048e01e9bca82202ddda15cf9ae80774e9d2
buffaloniandev <76931100+buffaloniandev@users.noreply.github.com>
2021-01-05 00:45:29 -0500

    Update downloader.lua

--- downloader.lua
+++ downloader.lua
@@ -1,6 +1,7 @@
 print("What is the URL for the script?")
-print("(e.g. https://example.com/script.lua)\n")
+print("(e.g. https://example.com/script.lua)")
 url = read()
+print("")
 handle = http.get(url)
 
 if not handle then
@@ -12,6 +13,7 @@ else
   print("What is the file path to save the script?")
   print("(e.g. /startup.lua)\n")
   path = read()
+  print("")
   
   handle = fs.open(path, 'w')
   handle.write(content)
